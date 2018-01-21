a4fd2c74f8eee7c3b6d90f59dd93ad8ab965a1ef
王宇飞 <1650043869@qq.com>
2018-01-21 18:42:35 +0800

    线程

--- Test11.py
+++ Test11.py
@@ -44,3 +44,55 @@ if __name__ == '__main__':
     p.close()
     p.join()
     print('all subprocesses done')
+
+# 多线程，python提供了2个模块，_thread和threading，大多情况下，只使用threading
+import time,threading
+def loop():
+    print ('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print ('thread %s >>> %s' % (threading.current_thread().name,n))
+        time.sleep(1)
+    print ('thread %s ended' % threading.current_thread().name)
+print ('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target = loop,name = 'LoopThread')
+t.start()
+t.join()
+print ('thread %s ended' % threading.current_thread().name)
+#current_thread()函数，永远返回当前线程的实例。
+
+# lock,多进程中，同一个变量，各自有一份拷贝存在于每个进程中，互不影响，多线程中，所有变量都由所有线程共享，任何一个变量都可以被任何一个线程修改。
+import time,threading
+balance = 0
+lock = threading.Lock()#用锁可以使一个变量仅有一个线程来修改，修改完后再继续执行
+def change_it(n):
+    global balance #共享变量
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        lock.acquire()#获取锁
+        try:
+            change_it(n)
+        finally:
+            lock.release()#释放锁
+
+t1 = threading.Thread(target = run_thread,args = (5,))
+t2 = threading.Thread(target = run_thread,args = (8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print (balance)
+
+import threading,multiprocessing
+def loop():
+    x = 0
+    while True:
+        x = x ^ 1
+
+for i in range(multiprocessing.cpu_count()):
+    t = threading.Thread(target = loop)
+    t.start()
